15a13db39da4716b8bf8545ad3d558989274aaec
MyroslavTiuk <myroslavtiukwork@gmail.com>
2023-02-01 14:20:08 +0200

    feat: second good commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,7 @@ export default function Home() {
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <p>Some change</p>
+          <p>Some change</p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
