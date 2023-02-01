74ff930d380774499a2fd1803e49e2a38fb15648
MyroslavTiuk <myroslavtiukwork@gmail.com>
2023-02-01 14:09:58 +0200

    feat: good commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,6 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
-          <p>dfsd</p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -38,7 +37,7 @@ export default function Home() {
                 priority
               />
             </a>
-            <p>jksdsd</p>
+            <>jksdsd</>
           </div>
         </div>
 
